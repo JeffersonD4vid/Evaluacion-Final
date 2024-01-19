152eb481f85aa5e6aa7a205127d533dada09caac
JeffersonD4vid <Jeffersonmd2002@gmail.com>
2024-01-18 20:12:26 -0500

    add

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
-let clickCambiarColor= document.getElementById("bloque1");
-let contEfect = document.querySelector("#bloque2");
+let clickCambiarColor= document.getElementById('bloque1');
+let contEfect = document.querySelector('#bloque2');
 
 clickCambiarColor.addEventListener('mouseover', ()=>{
     contEfect.style.backgroundColor = 'green';
