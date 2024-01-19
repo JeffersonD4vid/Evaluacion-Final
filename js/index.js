f6f306a340dc6abea789e9bea0a8b146352a4d6b
JeffersonD4vid <Jeffersonmd2002@gmail.com>
2024-01-18 20:07:48 -0500

    add

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+let clickCambiarColor= document.getElementById("bloque1");
+let contEfect = document.querySelector("#bloque2");
+
+clickCambiarColor.addEventListener('mouseover', ()=>{
+    contEfect.style.backgroundColor = 'Skyeblue';
+})
